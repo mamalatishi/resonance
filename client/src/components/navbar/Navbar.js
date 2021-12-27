44d375d1e0437a7f490f20ce2a7068a9d4e17aa1
mamalatishi <atishimamal@gmail.com>
2021-12-27 19:22:50 +0330

    built the navbar and set a new font

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import {HiOutlineShoppingCart,HiSearch,HiUser} from 'react-icons/hi';
+import './navbar.scss';
 
 function Navbar() {
+
+    function handleSearchButton(e) {
+        console.log('handleSearchButton')
+    }
     return (
-        <div>
-            Navbar
+        <div className=''>
+            <div className="container">
+            <div className="brand">
+                <Link to='/'>
+                    <h1>Resonance</h1>
+                </Link>
+            </div>
+            <button onClick={handleSearchButton}><HiSearch/></button>
+            <Link to='/user'><HiUser/></Link>
+            <Link to='/shoppingcard'><HiOutlineShoppingCart/></Link>
+            </div>
         </div>
     )
 }
 
 export default Navbar
+
+
