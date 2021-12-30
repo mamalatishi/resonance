15b0fb82fbd8d7d958239a2b937b41a9391732f7
mamalatishi <atishimamal@gmail.com>
2021-12-30 13:40:47 +0330

    made a new style to the header

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,44 +1,60 @@
 import React from 'react'
-import {useState} from 'react'
-import IMAGES from '../media/index'
+import {useState,useEffect} from 'react'
+import {IMAGES_header} from '../media/index'
 import {HiArrowSmLeft , HiArrowSmRight} from "react-icons/hi"
 import './header.scss';
 
-const initialImage = IMAGES[0]
+const images = IMAGES_header;
+
+
 
 function Header() {
-    const [img,setImg] = useState(initialImage);
+    const [index,setIndex] = useState(0);
+
+   // const interval = setInterval(() => {
+    //    let index = images.indexOf(img);
+    //    let next = index + 1;
+    //    index < lastIndex ? setImg(images[next]) : setImg(images[0]);
+   // } , 6000) 
+   
+
     const date = new Date();
-    let index = IMAGES.indexOf(img);
-    let lastIndex = IMAGES.length - 1;
+    let lastIndex = images.length - 1;
 
     function handleArrowClick(way) {
         if (way === 'right') {
-            console.log(date.getMonth());
-            index < lastIndex ? setImg(IMAGES[index + 1]) : setImg(IMAGES[0]);
+            index < lastIndex ? setIndex(index + 1) : setIndex(0);
         } else {
-            index == 0 ? setImg(IMAGES[lastIndex]) : setImg(IMAGES[index-1]);
+            index == 0 ? setIndex(lastIndex) : setIndex(index-1);
         }
     }
+
+
 
     return (
         <div className="header">
             <div className="container">
                 <div className="img-container">
-                    <img src={img.src} alt={img.name} />
-                    <div className="img-content">
-                        <div className="season">
-                            {`The ${date.getMonth()}th Season of ${date.getFullYear()}`}
-                        </div>
-                        <div className="img-title">
-                            <h2>Season Inclusive Bounties</h2>
-                        </div>
-                        <div className="img-description">
-                            <p>Limited stocks, New ones release the first of every month</p>
-                        </div>
+
+                    {images.map((img) => {
+                        console.log(img);
+                        const imgIndex = images.indexOf(img);
+                        return (<img className={imgIndex !== index ? `hidden img-${img.id}` : `img-${img.id}`} src={img.src} alt={img.name}/>)
+                    })}
+
+                </div>
+                <div className="img-content">
+                    <div className="season">
+                        {`The ${date.getMonth()}th Season of ${date.getFullYear()}`}
+                    </div>
+                    <div className="img-title">
+                        <h2>Season Inclusive Bounties</h2>
+                    </div>
+                    <div className="img-description">
+                        <p>Limited stocks, New ones release the first of every month</p>
+                    </div>
                     <button className="arrow-left" onClick={() => handleArrowClick('left')}><HiArrowSmLeft/></button>
                     <button className="arrow-right" onClick={() => handleArrowClick('right')}><HiArrowSmRight/></button>
-                </div>
                 </div>
                 
             </div>
---
+++ client/src/components/media/index.js
@@ -0,0 +1,79 @@
+import pic1 from './pic1.jpg'
+import pic2 from './pic2.jpg'
+import pic3 from './pic3.jpg'
+import pic4 from './pic4.jpg'
+import pic5 from './pic5.jpg'
+import pic6 from './pic6.jpg'
+import pic7 from './pic7.jpg'
+import pic8 from './pic8.jpg'
+import pic9 from './pic9.jpg'
+import pic10 from './pic10.jpg'
+import pic11 from './pic11.jpg'
+import pic12 from './pic12.jpg'
+import pic13 from './pic13.jpg'
+
+const IMAGES_header = [
+    {
+        id:1,
+        name:'pic1',
+        src:pic1
+    },
+    {
+        id:2,
+        name:'pic2',
+        src:pic2
+    },{
+        id:3,
+        name:'pic3',
+        src:pic3
+    },{
+        id:4,
+        name:'pic4',
+        src:pic4
+    }
+]
+
+const IMAGES = [
+    
+        {
+            id:5,
+            name:'pic5',
+            src:pic5
+        },{
+            id:6,
+            name:'pic6',
+            src:pic6
+        },{
+            id:7,
+            name:'pic7',
+            src:pic7
+        },{
+            id:8,
+            name:'pic8',
+            src:pic8
+        },{
+            id:9,
+            name:'pic9',
+            src:pic9
+        },{
+            id:10,
+            name:'pic10',
+            src:pic10
+        },{
+            id:11,
+            name:'pic11',
+            src:pic11
+        },{
+            id:12,
+            name:'pic12',
+            src:pic12
+        },{
+            id:13,
+            name:'pic13',
+            src:pic13
+        },
+    
+]
+
+export  {IMAGES_header};
+export default IMAGES;
